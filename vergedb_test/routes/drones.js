fcd7937da00d2a7b6d21a5a035b4532b6a43192a
gdonados <donado24@students.rowan.edu>
2022-06-15 15:03:00 -0400

    PUT and PATCH functionality added

--- vergedb_test/routes/drones.js
+++ vergedb_test/routes/drones.js
@@ -18,16 +18,31 @@ router.get('/:id', function (req, res) {
     res.json(drones.getSingle(req.params.id));
 });
 
+
+/* DELETE drones */
 router.delete('/:id', function (req, res) {
     res.json(drones.removeSingle(req.params.id));
+});
+
+/*PUT drones*/
+router.put('/:id', function (req, res, next) {
+    try {
+        res.json(drones.putSingle(req.body, req.params.id));
+    } catch (err) {
+        console.error(`Error while updating drones `, err.message);
+        next(err);
+    }
+});
+
+/* PATCH drones */
+router.patch('/:id', function (req, res) {
+    res.json(drones.patchSingle(req.body, req.params.id));
 });
 
 /* POST drones */
 router.post('/', function (req, res, next) {
     try {
         //Commands here
-        console.log(req.params);
-        console.log(req.body);
         res.json(drones.create(req.body));
     } catch (err) {
         console.error(`Error while adding drones `, err.message);
