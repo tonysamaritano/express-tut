55d5e39c30256a24639761aa05f41bd84dced901
Ryan-Oliver-2k <60796472+Ryan-Oliver-2k@users.noreply.github.com>
2022-06-21 09:54:44 -0400

    running into error in testing

--- vergedb_test/server.test.js
+++ vergedb_test/server.test.js
@@ -21,6 +21,7 @@ const seedDb = db => {
     // stmt.finalize();
 }
 
+/* GET Single id test*/
 test('get first entry', () => {
     seedDb(db);
     const res = request(server).get('/1');
@@ -32,3 +33,66 @@ test('get first entry', () => {
     expect(res.body).toEqual(response);
 });
 
+// /* POST test */
+// test('add drone', () => {
+//     db.serialize(async () => {
+//         seedDb(db);
+//         await request(server)
+//             .post('/')
+//             .send({ type: 1, name: 'X1-006', owner: 'Verge' });
+//         const res = await request(server).get('/');
+//         const response = [
+//             { type: 0, name: 'X1-001', owner: 'Verge' },
+//             { type: 1, name: 'X1-002', owner: 'Verge' },
+//             { type: 0, name: 'X1-003', owner: 'Verge' },
+//             { type: 1, name: 'X1-004', owner: 'Image' },
+//             { type: 0, name: 'X1-005', owner: 'Image' },
+//             { type: 1, name: 'X1-006', owner: 'Verge' }
+//         ]
+//         expect(res.status).toBe(200);
+//         expect(res.body).toEqual(response);
+//     })
+// });
+
+// /* PUT test */
+// test('update drone', () => {
+//     db.serialize(async () => {
+//         seedDb(db);
+//         await request(server)
+//             .put('/6')
+//             .send({ type: 1, name: 'X1-006', owner: 'Tony' });
+//         const res = await request(server).get('/6');
+//         const response = [
+//             { type: 1, name: 'X1-006', owner: 'Tony' }
+//         ]
+//         expect(res.status).toBe(200);
+//         expect(res.body).toEqual(response);
+//     })
+// });
+
+// /* PATCH test */
+// test('patch drone', () => {
+//     db.serialize(async () => {
+//         seedDb(db);
+//         await request(server)
+//             .patch('/6')
+//             .send({ name: 'Patch Name' });
+//         const res = await request(server).get('/6');
+//         const response = [
+//             { type: 1, name: 'Patch Name', owner: 'Tony' }
+//         ]
+//         expect(res.status).toBe(200);
+//         expect(res.body).toEqual(response);
+//     })
+// });
+
+// /* DELETE test */
+// test('delete drone', () => {
+//     db.serialize(async () => {
+//         seedDb(db);
+//         const res = await request(server).delete('/1');
+//         const response = [];
+//         expect(res.status).toBe(200);
+//         expect(res.body).toEqual(response);
+//     })
+// });
