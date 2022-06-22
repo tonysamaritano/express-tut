4d209db04d68ab057091d60d9f91eb029ea9722e
Ryan-Oliver-2k <60796472+Ryan-Oliver-2k@users.noreply.github.com>
2022-06-22 16:24:03 -0400

    error handling

--- vergedb_test/routes/drones.js
+++ vergedb_test/routes/drones.js
@@ -2,50 +2,79 @@ const express = require('express');
 const router = express.Router();
 const drones = require('../services/drones');
 
-/* GET drones listing. */
+/* GET drones listing */
 router.get('/', function (req, res, next) {
     try {
-        //Commands here
-        res.json(drones.getMultiple(req.query.page));
-        
+        res.json(drones.getAllDrones());        
     } catch (err) {
         console.error(`Error while getting drones `, err.message);
+        responseCode = drones.FindResponseCode(err);
+        res.status(responseCode).json({ error: err.message });
+        
+        // Next passes control to the next handler
+        // next('route') bypasses current remaining 
+        // route callbacks and jumps to a new route
         next(err);
     }
 });
 
+/* GET single drone by id */
 router.get('/:id', function (req, res) {
-    res.json(drones.getSingle(req.params.id));
+    try {
+        res.json(drones.getSingle(req.params.id));
+    } catch(err) {
+        console.error(err);
+        responseCode = drones.FindResponseCode(err);
+        res.status(responseCode).json({ error: err.message });
+    }
 });
-
 
 /* DELETE drones */
 router.delete('/:id', function (req, res) {
-    res.json(drones.removeSingle(req.params.id));
+    try {
+        res.json(drones.removeSingle(req.params.id));
+    } catch (err) {
+        console.error(err);
+        responseCode = drones.FindResponseCode(err);
+        res.status(responseCode).json({ error: err.message });
+    }
 });
 
-/*PUT drones*/
+/* PUT drones */
 router.put('/:id', function (req, res, next) {
     try {
         res.json(drones.putSingle(req.body, req.params.id));
     } catch (err) {
         console.error(`Error while updating drones `, err.message);
+        responseCode = drones.FindResponseCode(err);
+        res.status(responseCode).json({ error: err.message });
         next(err);
     }
 });
 
 /* PATCH drones */
 router.patch('/:id', function (req, res) {
-    res.json(drones.patchSingle(req.body, req.params.id));
+    try {
+        res.json(drones.patchSingle(req.body, req.params.id));
+    } catch (err) {
+        console.error(`Error while updating drones `, err.message);
+        responseCode = drones.FindResponseCode(err);
+        res.status(responseCode).json({ error: err.message });
+        next(err);
+    }
 });
 
 /* POST drones */
 router.post('/', function (req, res, next) {
     try {
-        //Commands here
         res.json(drones.create(req.body));
     } catch (err) {
         console.error(`Error while adding drones `, err.message);
+        responseCode = drones.FindResponseCode(err);
+        if(responseCode == 201)
+            res.status(responseCode).json({ success: err.message });
+        else
+            res.status(responseCode).json({ error: err.message });
         next(err);
     }
 });
--- vergedb_test/services/drones.js
+++ vergedb_test/services/drones.js
@@ -1,33 +1,66 @@
 const db = require('../services/db');
-const config = require('../config');
+// const config = require('../config');
 
-function getMultiple(page = 1) {
-    const offset = (page - 1) * config.listPerPage;
-    const data = db.query(`SELECT * FROM drones LIMIT ?,?`, [offset, config.listPerPage]);
-    const meta = { page };
+// enumeration for different errors
+const ErrorCodes = {
+    CreatedSuccessfully:    0,     // code: 201
+    BadRequest:            -1,     // code: 400
+    NotFound:              -2,     // code: 404
+}
 
-    return {
-        data,
-        meta
+function DataBaseError(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    return error;
+}
+
+function FindResponseCode(err) {
+    // return code 200 ('OK') as default
+    let responseCode = 200;
+    switch(err.code){
+        default: 
+        case ErrorCodes.CreatedSuccessfully:
+            responseCode = 201;    
+        break;
+        case ErrorCodes.BadRequest:
+            responseCode = 400;
+        break;
+        case ErrorCodes.NotFound: 
+            responseCode = 404;
+        break;
     }
+    return responseCode;
+}
+
+/*
+    @return: a list of all drones with no limit
+*/
+function getAllDrones() {
+    const data = db.query(`SELECT * FROM drones`, []);
+    if(!data.length){
+        throw DataBaseError(`No drones found in database`, ErrorCodes.NotFound)
+    }
+    return data
 }
 
 function getSingle(number) {
     var data = db.query(`SELECT * FROM drones WHERE id=?`,number);
-
+    if(!data.length){
+        throw DataBaseError(`Drone with id ${number} does not exist`, ErrorCodes.NotFound);
+    }
     return data
 }
 
 function removeSingle(number) {
     var data = db.run(`DELETE FROM drones WHERE id=?`, number);
-
+    if(!data.changes){
+        throw DataBaseError(`Drone with id ${number} does not exist`, ErrorCodes.NotFound);
+    }
     return data
 }
 
 function validateCreate(droneObj) {
     let messages = [];
-
-    console.log(droneObj.type);
 
     if (!droneObj)
         messages.push('No object is provided');
@@ -40,35 +73,32 @@ function validateCreate(droneObj) {
 
     if (!droneObj.owner)
         messages.push('Owner is empty');
-
-    if (messages.length) {
-        let error = new Error(messages.join());
-        error.statusCode = 400;
-
-        throw error;
+    
+    if(messages.length){
+        throw DataBaseError(messages.join(), ErrorCodes.BadRequest);
     }
+    
+    return messages;
 }
 
 function putSingle(droneObj, number) {
-    let errMsg = [];
+    let message = 'Drone updated successfully';
 
-    validateCreate(droneObj);
-    
-    if (errMsg.length) {
-        let error = new Error(errMsg.join());
-        error.statusCode = 400;
+    // if validation was succesful
+    if (!validateCreate(droneObj).length) {
+        var data = db.run(`UPDATE drones
+        SET type=?, name=?, owner=?
+        WHERE id=?`, [droneObj.type, droneObj.name, droneObj.owner, number]);
 
-        throw error;
+        // if no changes occured, drone did not update successfully
+        // can probably delete, this is just a catch all
+        if (!data.changes) {
+            message = 'Error updating drone'
+            throw DataBaseError(`Drone with id ${number} did not update successfully`, 
+                                ErrorCodes.BadRequest);
+        }
     }
 
-    var data = db.run(`UPDATE drones
-                SET type=?, name=?, owner=?
-                WHERE id=?`, [droneObj.type, droneObj.name, droneObj.owner, number]);
-
-    let message = 'Error updating drone';
-    if (data.changes)
-        message = 'Drone updated successfully';
-    
     return {
         data,
         message
@@ -91,27 +121,34 @@ function patchSingle(droneObj, number) {
                 SET type=?, name=?, owner=?
                 WHERE id=?`, [type, name, owner, number]);
 
+    if(!data.changes) {
+        throw DataBaseError(`Drone with id ${number} does not exist`, ErrorCodes.NotFound);
+    }
     return data
 }
 
 function create(droneObj) {
-    validateCreate(droneObj);
-    const { type, name, owner } = droneObj;
-    const result = db.run('INSERT INTO drones (type, name, owner) VALUES (@type, @name, @owner)', { type, name, owner });
-
     let message = 'Error in creating drones';
-    if (result.changes) {
-        message = 'Drone added successfully';
+
+    if(!validateCreate(droneObj).length) {
+        const { type, name, owner } = droneObj;
+        const result = db.run('INSERT INTO drones (type, name, owner) VALUES (@type, @name, @owner)',
+                             { type, name, owner });    
+        if (result.changes) {
+            message = 'Drone added successfully';
+            throw DataBaseError(`Drone added successfully`, ErrorCodes.CreatedSuccessfully);
+        }
     }
 
     return message;
 }
 
 module.exports = {
-    getMultiple,
+    FindResponseCode,
+    getAllDrones,
     getSingle,
     removeSingle,
     putSingle,
     patchSingle,
-    create
+    create,
 }
